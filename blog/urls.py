3743ef43c87ad4d665d778f050ecb4c24be1c041
Denis <sairustv228@gmail.com>
2026-04-20 21:58:08 +0300

    Solution

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog.views import IndexView, PostDetailView, SendCommentView
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", SendCommentView.as_view(), name="send-comment"),
+]
+
+app_name = "blog"
