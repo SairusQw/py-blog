0bb608bb7caf71640f0cf189b4c4dd1e3ff0a682
Denis <sairustv228@gmail.com>
2026-04-27 20:26:26 +0300

    Fix

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from blog.views import IndexView, PostDetailView, SendCommentView
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/int:pk/comment/", SendCommentView.as_view(), name="send-comment"),
+    path("posts/int:pk/comment/", SendCommentView.as_view(),
+         name="send-comment"),
 ]
 
 app_name = "blog"
